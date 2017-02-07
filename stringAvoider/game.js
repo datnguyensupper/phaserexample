404ad40b0b1e9e9beb43ad7081ca0b847054a396
donick\tuynu <dat.nguyen.supper@gmail.com>
2017-02-07 18:58:04 +0700

    update string avoid project

--- stringAvoider/game.js
+++ stringAvoider/game.js
@@ -24,6 +24,9 @@ var mainState = {
         game.scale.pageAlignVertically = true;
 
         game.load.image("maze", "assets/maze.png");
+
+        // preloading start and end icons as spritesheet
+        game.load.spritesheet("startend", "assets/startend.png", 80, 80);
     },
 
     create:function() {
@@ -39,21 +42,100 @@ var mainState = {
         // adding the bitmap data as a sprite
         game.add.sprite(0,0,this.bitmap);
 
-        // segments is the array which will cotnain string segments
-        this.segments=[];
+        // adding start icon
+        this.startSpot = game.add.sprite(80, 80, "startend", 0);
 
-        // populating segments array with gameOptions.tailSegments Point
-        for(var i = 0; i < gameOptions.tailSegments; i++){
-            this.segments[i] = new Phaser.Point(0,0);
-        }
+        // setting start icon registration point to its centre
+        this.startSpot.anchor.set(0.5);
+
+        // adding end icon
+        this.endSpot = game.add.sprite(game.width - 80,game.height - 80, "startend", 1);
+
+        // setting start icon registration point to its centre
+        this.endSpot.anchor.set(0.5);
+
+        // just a flag to inform us if we already had an input, that is if the player already clicked/touched the canvas
+        this.firstInput = true;
+
+        //// populating segments array with gameOptions.tailSegments Point
+        //for(var i = 0; i < gameOptions.tailSegments; i++){
+        //    this.segments[i] = new Phaser.Point(0,0);
+        //}
 
         // we create a graphics instance called "canvas", we'll draw the string on it
         this.canvas = game.add.graphics(0,0);
 
+        // segments is the array which will cotnain string segments
+        this.segments=[];
+
+        // waiting for player input to call startMove method
+        game.input.onDown.add(this.startMove, this);
+
     },
 
-    // at each update
-    update: function(){
+    // startMove method, will be called each time the player touches/clicks the canvas
+    startMove: function(e){
+        // checking if it's the first input: player clicks/touchs the canvas for the first time
+        if(this.firstInput){
+            // not the first input anymore
+            this.firstInput = false;
+
+            // making start icon invisible
+            this.startSpot.visible = false;
+
+            // populating segments array with an amount of "gameOptions.tailSegments" Phaser Point objects
+            for(var i = 0; i < gameOptions.tailSegments; i++){
+                // I want the string to be a circle at first, so I am using a little trigonometry to place thế points accordingly
+                var radians = 12 * Math.PI*i/gameOptions.tailSegments + Math.PI/4;
+                // creatingh Points objects and placing them into segments array. "10" is the radius of the circle
+                this.segments[i] = new Phaser.Point(this.startSpot.x+10*Math.cos(radians),this.startSpot.y+10*Math.sin(radians));
+            }
+
+            // calling moveString function. Actually this function moves and renders the string, and the two arguments represent
+            // respectively the x and y movement to apply to string's head. We set them to zero because there's no movement
+            this.moveString(0,0);
+        }
+
+        // removing callback
+        game.input.onDown.remove(this.startMove, this);
+
+        // add move callback to be fired when the player moves the mouse/finger and call dragString method
+        game.input.addMoveCallback(this.dragString, this);
+
+        // add a up callback to be fired when the player release the finger/mouse button and call endMove method
+        game.input.onUp.add(this.endMove, this);
+
+        // saving current event position, that is the position where the player is currently touching/clicking
+        this.startPosition = e.position;
+    },
+
+    // endMove method is called when the player released the finger/the mouse button
+    endMove: function(){
+        // waiting for player input to call startMove method
+        game.input.onDown.add(this.startMove, this);
+
+        // removing other listeners
+        game.input.onUp.remove(this.endMove, this);
+        game.input.deleteMoveCallback(this.dragString, this);
+    },
+
+    // dragString method is called when the player the player moves the finger or the mouse while keeping mouse button pressed
+    dragString: function(e){
+        // calling moveString function. Actually this function moves and renders the string, and the two arguments represent
+        // respectively the x and y movement to apply to string's head.
+        // We set them to represent the distance from current input position and previous input position
+        this.moveString(e.position.x - this.startPosition.x, e.position.y-this.startPosition.y);
+
+        // updating startPosition variable
+        this.startPosition = new Phaser.Point(e.position.x, e.position.y);
+
+    },
+
+    // moveString method updates and renders the string
+    moveString: function(x,y){
+
+        // it's
+
         // clearing the canvas, ready to be redrawn
         this.canvas.clear();
 
@@ -83,11 +165,14 @@ var mainState = {
             // if the color alpha is different than zero, that is it's not a transparent pixel...
             if(color.a != 0){
                 // from now on ,draw the string in red
-               this.canvas.lineStyle(4, 0xff0000,1);
+                this.canvas.lineStyle(4, 0xff0000,1);
             }
 
             // drawing the segment
             this.canvas.lineTo(this.segments[i].x, this.segments[i].y);
         }
+
     },
+
+
 };
